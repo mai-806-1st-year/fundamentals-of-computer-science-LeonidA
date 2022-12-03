ca4854483e2a7680d53c3b810942644bfb7d3a51
leniks <94530828+leniks@users.noreply.github.com>
2022-12-03 15:39:13 +0300

    Add files via upload

--- lab13.c
+++ lab13.c
@@ -15,13 +15,17 @@ uint char_to_set(char c)
     {
         return 0;
     }else{
-        return 1u<<(c-'a');
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+            return 1u<<(c-'a');
+        } else{
+            return 0;
+        }
     }
 }
 
 bool logic(char * word){
     
-    bool flag;
+    bool flag = false;
     uint set_word_vowels = 0, letter;
     int a = 0;
     
@@ -36,7 +40,9 @@ bool logic(char * word){
         }
         
         set_word_vowels = set_word_vowels | letter;
-        flag = true;
+        if (set_word_vowels != 0) {
+            flag = true;
+            }
         a++;
     }
     return flag;
@@ -47,29 +53,38 @@ void main()
     bool flag = false;
     char symbol, word[30] = "";
     symbol = getchar();
+    strncat(word, &symbol, 1);
     
     while (symbol != EOF) {
         
-        if(symbol == 32 || symbol == 10 || symbol == 44 || symbol == 9){
+        if((symbol == 32 || symbol == 10 || symbol == 44 || symbol == 9) && word[0] != 0 && word[0] != 32 && word[0] != 10 && word[0] != 44 && word[0] != 9 )
+        {
             strcat(word, "1");
             flag = logic(word);
             if (flag == true){
                 break;
             }
-            
+            memset(word, 0, 30);
+            symbol = getchar();
         }
         
         else {
-            strncat(word, &symbol, 1);
+            if (word[0] == 0 || word[0] == 32 || word[0] == 10 || word[0] == 44 || word[0] == 9)
+            {
+                memset(word, 0, 30);
+            }
+            
             symbol = getchar();
+            strncat(word, &symbol, 1);
         }
     }
     if (flag == true) {
-        printf("word was found");
+        printf("word was found\n");
+        printf("%s", word);
     }
 
     if (flag == false) {
-        printf("word wasn't found");
+        printf("word wasn't found\n");
     }
     
     getch();

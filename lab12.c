9ad45956909bfece8b1f2b428205afd9c7967e37
leniks <94530828+leniks@users.noreply.github.com>
2022-12-09 02:06:52 +0300

    Add files via upload

--- lab12.c
+++ lab12.c
@@ -2,38 +2,59 @@
 #include <conio.h>
 #include <string.h>
 
-void main() {
-    char symbol, number[12] = "";
-    symbol = getchar();
-
-    while (symbol != EOF)
-    {
-        strncat(number, &symbol, 1);
-        symbol = getchar();
-    }
+void number_print(char number[12]){
     int size = strlen(number);
     
     if (number[0] == 45) {
         size -= 1;
-        if (size % 2 != 1)
+        if (size % 2 == 1)
     {
-        size = size / 2;
+        size = (size / 2) + 1;
         memmove(&number[size], &number[size + 1], strlen(number) - size);
     }
     
-    printf("%s", number);
+    printf("%s\n", number);
 
     }
     
     else {
-    if (size % 2 != 1)
+    if (size % 2 == 1)
     
     {
-    size = (size / 2) - 1;
+    size = size / 2;
     memmove(&number[size], &number[size + 1], strlen(number) - size);
     }
     
-    printf("%s", number);
+    printf("%s\n", number);
     };
+}
+
+void main() {
+    char symbol, number[12] = "";
+    symbol = getchar();
+    strcat(number, &symbol);
+
+    while (symbol != EOF)
+    {
+        if ((symbol == '\n' || symbol == '\t' || symbol == ' ' || symbol == ',') && number[0] != 0 && number[0] != '\n' && number[0] != '\t' && number[0] != ' ' && number[0] != ',')
+        {
+            number_print(number);
+            memset(number, 0, 12);
+        }
+
+        else
+        {
+            if ((number[0] == '\n' || number[0] == '\t' || number[0] == ' ' || number[0] == ',' || number[0] == 0) && number[0] != '-')
+            {
+                memset(number, 0, 12);
+            }
+
+            symbol = getchar();
+            if((symbol != '\n' && symbol != '\t' && symbol != ' ' && symbol != ',')){
+                strncat(number, &symbol, 1);
+            }
+        }
+    }
+    
     getch();
 }
